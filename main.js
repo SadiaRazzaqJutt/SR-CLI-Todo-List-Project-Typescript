083a1691bdd33e801c2a683e245822b05b4f1c53
SadiaRazzaqJutt <sadiarazzaq14@gmail.com>
2024-07-07 17:51:39 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "todo",
+            type: "input",
+            message: "Please enter todo tasks:"
+        },
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "Do you want to add other task in the list:",
+            default: "false"
+        }
+    ]);
+    todos.push(addTask.todo);
+    condition = addTask.addMore;
+    console.log(todos);
+}
